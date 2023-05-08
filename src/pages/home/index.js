dec0014e0567d31fba94a22144afd37c6b9cb45c
Geodor Ruales <geodorruales@gmail.com>
2023-05-09 05:04:58 +0800

    feat: add function to get territories

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,12 +4,36 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Home() {
+  const router = useRouter();
+  const [territories, setTerritories] = useState([]);
+
+  useEffect(() => {
+    getTerritories();
+  }, []);
+
+  const getTerritories = async () => {
+    return await axios
+      .get("https://nezwelt-server.herokuapp.com/Territories/All", {
+        headers: {
+          Authorization: `Bearer ${Cookies.get("jwt")}`,
+        },
+      })
+      .then((res) => setTerritories(res.data.territories))
+      .catch((e) => {
+        if (e.response.status === 403) router.push("/account/login");
+      });
+  };
+
+  
+
   
 
   return (
     <>
       <main>
-        
+        <h1>Territories</h1>
+        <h3>Here are a list of territories:</h3>
+       
       </main>
     </>
   );
