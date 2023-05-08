e881240287e381050c0abe46a8988e339bac2445
Geodor Ruales <geodorruales@gmail.com>
2023-05-09 05:04:18 +0800

    feat: import dependencies to home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,7 @@
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
   
