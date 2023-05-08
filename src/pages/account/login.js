3e8d46e0eca4806e20d187775034aa1677b199fd
Geodor Ruales <geodorruales@gmail.com>
2023-05-09 05:02:50 +0800

    feat: add function to handle login

--- src/pages/account/login.js
+++ src/pages/account/login.js
@@ -1,7 +1,37 @@
-
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Login() {
-  
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    loginUser();
+  };
+
+  const loginUser = async () => {
+    const data = await axios
+      .post("https://nezwelt-server.herokuapp.com/Account/SignIn", {
+        username,
+        password,
+      })
+      .then((res) => res.data)
+      .catch((e) => {
+        if (e.response.status === 401) alert("Invalid Password");
+        if (e.response.status === 404) alert("No Such User");
+      });
+
+    if (data) {
+      const { token } = data;
+
+      Cookies.set("jwt", token);
+      router.push("/home");
+    }
+  };
 
   return (
     <>
